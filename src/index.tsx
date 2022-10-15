8c5d9bdeaa01d1dce965a065d5cd4178cd7eec2a
Mykyta Lystopad <listopad.nikita96@gmail.com>
2022-10-15 22:30:12 +0300

    add router v6 to check

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,26 @@ import App from "./App";
 import {Provider} from "react-redux";
 import {store} from "./store";
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: 'test',
+        element: <h1>Hello test route</h1>
+    }
+]);
+
+// https://reactrouter.com/en/main/start/tutorial
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 )
 root.render(
     <Provider store={store}>
-            <App/>
+            <RouterProvider router={router}/>
     </Provider>
 )
